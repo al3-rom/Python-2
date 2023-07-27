9ec011369075e68a80c52e85df19750128c1126f
al3-rom <sasha.stankevich223@gmail.com>
2023-07-27 17:31:32 +0200

    Actualizando ejercicios

---
+++ Python_2A/BienvenidaUser.py
@@ -0,0 +1,22 @@
+# Un ordenador tiene 3 usuarios(Alejandro, Naomi y Sergio) y otro usuario invitado
+# 1. Pedir el nombre al usuario y dar una bienvenida personalizada.
+# *(Si el usuario no es ninguno de los tres, se le de un saludo generico)
+# !Prevenir error si el nombre es en minusculas, mayusculas, con punto y con almohadilla!
+
+user1 = "Alejandro"
+user2 = "Naomi"
+user3 = "Sergio"
+invitado = input("Hola, pon tu nombre!: ")
+
+
+if invitado == user1:
+    print("Buenas dias ", user1, ", te doy un saludo!")
+elif invitado == user2:
+   print("Buenas dias ", user2, ", te doy un saludo!")
+elif invitado == user3:
+   print("Buenas dias ", user3, ", te doy un saludo!")
+else:
+   print("Buenas dias", invitado,"!")
+
+
+
