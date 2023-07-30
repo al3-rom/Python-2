b231d570df79079b0d6892b5473b10e66e9a5eb2
al3-rom <sasha.stankevich223@gmail.com>
2023-07-30 17:32:47 +0200

    Actualizando ejercicios

--- Python_2B/MayusculaOMinuscula.py
+++ Python_2B/MayusculaOMinuscula.py
@@ -1,7 +1,10 @@
 # Permitir que el usuario introduzca una letra(del alfabeto latino) como input. Comprombar si esta es una mayuscula o una minuscula
 
+
+# Pedimos una letra
 letra = input("Pon una letra: ")
 
+# Comprobamos si en una mayuscula o minuscula
 if letra.islower():
     print("La letra:", letra, "es una minuscula")
 elif letra.isupper():
