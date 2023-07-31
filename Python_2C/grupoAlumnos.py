e774e37f150cf9ad9269ebd87183545accaf7111
al3-rom <sasha.stankevich223@gmail.com>
2023-07-31 16:38:21 +0200

    Actualizando nombre de la carpeta

---
+++ Python_2C/grupoAlumnos.py
@@ -0,0 +1,16 @@
+# En uno de los cursos se ha dividido a una clase en dos grupos A y B.
+# Para mezclar a los alumnos lo mejor posible se ha asignado a todas las chicas 
+# con nombres empezando por letra E hasta la M en el grupo A y el resto en B
+# A los chichos con nombre empezando por letra A hasta la H y R hasta la Z se les ha asignado al grupo A timbien, el resto estan en el B
+# 
+# Preguntar al usuario si es chica o chico y el nombre.
+# Hay que mostrar por pantalla el grupo que le corresponde a ese alumno
+
+# Preguntamos si es chica o chico y su nombre
+
+genero = input("Eres chica o chico?: ")
+nombre = input("Dime tu nombre: ")
+primeraLetra = nombre[0]
+
+# Mostramos el grupo que le corresponde
+
